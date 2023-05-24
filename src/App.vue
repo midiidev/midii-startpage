80f3d58eda05d0c1f3ecb5e012e3b581ddb64843
Ashlyn <ash@midiidev.xyz>
2023-05-24 13:03:34 -0400

    install frameworks and initailise git

---
+++ src/App.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1>hello world</h1>
+  <h2 class="text-primary">hello daisyui</h2>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      './index.html',
+      './src/**/*.{vue,js}'
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: [
+      require('daisyui')
+  ]
+}
+
